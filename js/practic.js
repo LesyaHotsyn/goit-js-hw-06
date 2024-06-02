a7065229ccd3f6bd79f121d07bcdd1d849e4e77a
Hotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2024-06-02 21:05:28 +0300

    add task 1

---
+++ js/practic.js
@@ -0,0 +1,23 @@
+class Car {
+  constructor(params) {
+    this.brand = params.brand;
+    this.model = params.model;
+    this.price = params.price;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  getBrand() {
+    return this.brand;
+  }
+
+  changeBrand(newBrand) {
+    this.brand = newBrand;
+  }
+}
